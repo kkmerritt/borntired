a5536bc780cc362381ac37858bccee758d9c7410
Kevin Merritt <kkmerritt@gmail.com>
2016-02-04 17:39:46 -0500

    event listeners issue, creating dead links

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,10 +1,1 @@
-angular.module('borntired')
-.config(function('$routeProvider'){
-  $routeProvider
-  .when('/notes',{
-    templateUrl: '/templates/pages/notes/index.html',
-  })
-  .when('/users',{
-    templateUrl: '/templates/pages/users/index.html',
-  })
-});
+an
