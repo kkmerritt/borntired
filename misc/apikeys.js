34cae1b82bbaedfe76a6ecb4e516b8fda3b57990
Kevin Merritt <kkmerritt@gmail.com>
2016-02-24 22:50:08 -0500

    heroku api key upload

---
+++ misc/apikeys.js
@@ -0,0 +1,5 @@
+//NOTE  ----------------apikeys [gitignored]-----------------//
+
+var sendgrid_api_key = "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";
+
+module.exports = sendgrid_api_key;
