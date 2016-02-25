cec9fb4b8ea2d0cf75a16a87f1e0f11ed90dff0c
Kevin Merritt <kkmerritt@gmail.com>
2016-02-24 22:52:19 -0500

    Delete apikeys.js

--- misc/apikeys.js
+++
@@ -1,5 +0,0 @@
-//NOTE  ----------------apikeys [gitignored]-----------------//
-
-var sendgrid_api_key = "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";
-
-module.exports = sendgrid_api_key;
