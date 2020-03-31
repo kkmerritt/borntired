7f6695a335afcba1dc0ced0e4fd642db3ec45941
kev merritt <kkmerritt@gmail.com>
2020-03-30 20:49:59 -0400

    removed the db, server, more unused files/code

--- server.js
+++ server.js
@@ -3,22 +3,15 @@
 // NOTE: ----------------------  Server.js
 
 var express         = require('express');
+    server = express();
 var    ejs          = require('ejs');
 var   bodyParser    = require('body-parser');  // pull information from HTML POST (express4)
 var  methodOverride = require('method-override');  // simulate DELETE and PUT (express4)
 var  morgan         = require('morgan');    // log requests to the console (express4)
 var  session        = require('express-session');
-// var  User           = require('./models/user.js');
 var  logger         = require('./misc/logger.js');
 var  eyes           = require('eyespect');
 var mongoose        = require('mongoose');
-var sendgrid_api_key = process.env.sendgrid_api_key;
-var sendgrid        = require('sendgrid')(sendgrid_api_key);
-
-
-PORT = process.env.PORT || 3000, server = express(),
-MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017/users",
-  dbname = "borntired", mongoose = require('mongoose');
 
 // NOTE: ---------------------- Activate / Use Middleware
 
@@ -38,38 +31,13 @@ server.use(morgan('dev'));
 server.use(logger);
 
 
-// NOTE: ---------------------- DB Models
- var Article = mongoose.model('Article', {
-     text : String
- });
-
-
-// NOTE: ---------------------- Server & Database Connections
-mongoose.connect(MONGOURI + "/" + dbname)
-server.listen(PORT,function(){console.log("SERVER IS UP ON PORT:", PORT);})
-
-var db = mongoose.connection;
-db.on('error', function(){console.log("DATABASE: CONNECTION ERROR: for fuck's sake. " + dbname)})
-db.once('open', function(){console.log("DATABASE: CONNECTED: " + dbname)})
-
-
 // NOTE: ---------------------- Server Routes
 
-server.get('/', function(req, res){res.render('index');});
-
-server.post('/', function(req,res){
-
-var sendgrid  = require('sendgrid')(sendgrid_api_key);
-sendgrid.send({
-  to:       process.env.EMAIL,
-  from: req.body.email,
-  subject:  'borntired email',
-  text:     req.body.name + " - " + req.body.message
-
-}, function(err, json) {
-  if (err) {res.render('404');}
-  else {res.render('messagesent');}
-  console.log(json);
+server.get('/',
+function(req, res){
+  res.render('index');
 });
 
-});
+
+
+server.listen(3000, console.log("SERVER IS UP ON PORT: 3000"))
